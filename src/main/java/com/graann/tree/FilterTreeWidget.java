050c36f13e9036f136e60b2d8d996e94f57461a8
graann <graann@gmail.com>
2017-09-22 00:28:50 +0300

    wrapper

--- src/main/java/com/graann/tree/FilterTreeWidget.java
+++ src/main/java/com/graann/tree/FilterTreeWidget.java
@@ -9,16 +9,21 @@ import rx.schedulers.Schedulers;
 
 import javax.swing.*;
 import javax.swing.tree.TreeModel;
+import javax.swing.tree.TreeNode;
+import javax.swing.tree.TreePath;
 import java.awt.*;
+import java.util.Enumeration;
 
 /**
  * @author gromova on 20.09.17.
  */
 public class FilterTreeWidget implements Viewable<JComponent> {
     private JPanel panel;
-    private JTree jTree;
+    private JTree tree;
     private JScrollPane scrollPane;
+
     private JTextField jTextField = new JTextField();
+    private JButton button = new JButton("expand");
 
     private FilterTreeModelWrapper factory;
 
@@ -32,7 +37,7 @@ public class FilterTreeWidget implements Viewable<JComponent> {
 
     public void initialize() {
 
-        panel = new JPanel(new MigLayout("fill, flowy", "", "[min!][]"));
+        panel = new JPanel(new MigLayout("fill, wrap 2", "[min!][]", "[min!][]"));
 
         panel.setPreferredSize(new Dimension(800, 600));
         panel.add(jTextField, "wmin 100");
@@ -41,15 +46,41 @@ public class FilterTreeWidget implements Viewable<JComponent> {
             jTextField.addActionListener(e -> subscriber.onNext(jTextField.getText()));
         });
 
+        button.addActionListener(e -> expandAllNodes());
+        panel.add(button);
+
         TreeLoader.loadTree()
                 .subscribeOn(Schedulers.from(SwingUtilities::invokeLater))
                 .subscribe(model -> {
                     TreeModel filterTreeModel = factory.wrap(model, filterObservable);
 
-                    jTree = new JTree(filterTreeModel);
-                    scrollPane = new JScrollPane(jTree);
-
-                    panel.add(scrollPane, "grow");
+                    tree = new JTree(filterTreeModel);
+                    scrollPane = new JScrollPane(tree);
+                    panel.add(scrollPane, "grow, span 2");
                 });
     }
+
+    public void expandAllNodes() {
+        TreeNode root = (TreeNode) tree.getModel().getRoot();
+/*        TreeUI ui = tree.getUI();
+        tree.setUI(new BasicTreeUI());*/
+
+        expandAll(tree, new TreePath(root));
+
+       // tree.setUI(ui);
+    }
+
+    private void expandAll(JTree tree, TreePath parent) {
+        TreeNode node = (TreeNode) parent.getLastPathComponent();
+        if (node.getChildCount() >= 0) {
+            for (Enumeration e = node.children(); e.hasMoreElements(); ) {
+                TreeNode n = (TreeNode) e.nextElement();
+                TreePath path = parent.pathByAddingChild(n);
+                expandAll(tree, path);
+            }
+        }
+        tree.expandPath(parent);
+        // tree.collapsePath(parent);
+    }
 }
+
