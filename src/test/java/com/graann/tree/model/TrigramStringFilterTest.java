e2343c30800c6ea715a5f31bf6a0c24fde1e95e0
Anna Gromova <gromova@devexperts.com>
2017-09-26 17:36:26 +0300

    try to lazy expansion.

--- src/test/java/com/graann/tree/model/TrigramStringFilterTest.java
+++ src/test/java/com/graann/tree/model/TrigramStringFilterTest.java
@@ -1,17 +1,49 @@
 package com.graann.tree.model;
 
-import org.junit.jupiter.api.Test;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
 
+import java.util.Arrays;
+import java.util.HashSet;
 import java.util.Set;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
 
 /**
- * @author gromova on 25.09.17.
+ * @author gromova on 26.09.17.
  */
-class TrigramStringFilterTest {
+public class TrigramStringFilterTest {
+	private StringFilter trigramStringFilter;
+
+	@Before
+	public void setUp() throws Exception {
+		Set<String> strings = new HashSet<>();
+		strings.addAll(Arrays.asList("колбаса", "столб", "яблоко", "аккорд", "полоса", "самолет"));
+		trigramStringFilter = new StringFilterFactory().create(strings);
+	}
+
+	@After
+	public void tearDown() throws Exception {
+
+	}
+
 	@Test
-	void getNGrams() {
+	public void appropriateStringObservable() throws Exception {
+		trigramStringFilter.appropriateStringObservable("са")
+				.first()
+				.subscribe(strings -> {
+					assertTrue(strings.contains("колбаса"));
+					assertTrue(strings.contains("полоса"));
+					assertTrue(strings.contains("самолет"));
+				});
+	}
+
+	@Test
+	public void getNGrams() throws Exception {
 		String[] trigrams = {"кол", "олб", "лба", "бас", "аса", "са", "а"};
 
 		Set<String> set = TrigramStringFilter.getNGrams("колбаса", 3);
